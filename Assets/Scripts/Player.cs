1f8904e2011384b406f9422c93c3602e3ff21d5b
Shota <Shota@SHOTA-THINK>
2016-08-19 00:13:37 +0900

    プロジェクト ファイルを追加します。

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+    Spaceship spaceship;
+
+    IEnumerator Start()
+    {
+        spaceship = GetComponent<Spaceship>();
+
+        while (true)
+        {
+            spaceship.Shot(transform);
+            yield return new WaitForSeconds(spaceship.shotDelay);
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        Vector2 direction = new Vector2(x, y).normalized;
+
+        spaceship.Move(direction);
+
+	}
+}
